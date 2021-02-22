6c6ec61a9518b7d43874c71b2c23080e285416e3
codershuvo <nisho.mushfiq2020@gmail.com>
2021-02-22 21:43:07 +0600

    null vs undefined cleared

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+undefined
+
+let pakhi; 
+
+function add (num1, num2) {
+    console.log(num1 + num2); 
+    return
+}
+
+function add (num1, num2) {
+    console.log(num1, num2); 
+}
+const result = add(13); 
+
+const premik = {
+    name:"smart dude", 
+    phone:452827
+}; 
+console.log(premik.gf); 
+
+let fun = undefined; 
+let ages = [2, 5, 9]; 
+console.log(ages[11]); 
+
+
+
+
+null
+
